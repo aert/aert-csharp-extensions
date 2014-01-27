4b000a6c788b4300661da7244947f228f0583370
aert <dev.aert@gmail.com>
2014-01-27 18:20:57 +0100

    + IdentifierHelper

---
+++ aert-csharp-extensions.tests/IdentifierHelperTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aert_csharp_extensions.tests
+{
+    [TestClass]
+    public class IdentifierHelperTest
+    {
+        [TestMethod]
+        public void UtcNowTicks()
+        {
+            long idOne = IdentifierHelper.UtcNowTicks;
+            long idTwo = IdentifierHelper.UtcNowTicks;
+            Assert.IsTrue(idOne < idTwo);
+        }
+
+        [TestMethod]
+        public void DecimalToArbitrarySystem()
+        {
+            const long number = 635264270564339393;
+            string result = IdentifierHelper.DecimalToArbitrarySystem(number, 80);
+            Assert.AreEqual("4wpy<5j$2X", result);
+        }
+
+        [TestMethod]
+        public void ArbitraryToDecimalSystem()
+        {
+            const string number = "4wpy<5j$2X";
+            long result = IdentifierHelper.ArbitraryToDecimalSystem(number, 80);
+            Assert.AreEqual(635264270564339393, result);
+        }
+    }
+}
---
+++ aert-csharp-extensions/IdentifierHelper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace aert_csharp_extensions
+{
+    public class IdentifierHelper
+    {
+        private static long _LastTimeStamp = DateTime.UtcNow.Ticks;
+
+        public static long UtcNowTicks
+        {
+            get
+            {
+                long orig, newval;
+                do
+                {
+                    orig = _LastTimeStamp;
+                    long now = DateTime.UtcNow.Ticks;
+                    newval = Math.Max(now, orig + 1);
+                } while (Interlocked.CompareExchange
+                             (ref _LastTimeStamp, newval, orig) != orig);
+
+                return newval;
+            }
+        }
+
+        /// <summary>
+        /// Converts the given decimal number to the numeral system with the
+        /// specified radix (in the range [2, 80]).
+        /// </summary>
+        /// <param name="number">The number to convert.</param>
+        /// <param name="radix">The radix of the destination numeral system (in the range [2, 80]).</param>
+        /// <returns></returns>
+        public static string DecimalToArbitrarySystem(long number, int radix)
+        {
+            const int bitsInLong = 64;
+            const string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/-*()[]<>?!^{}&$=";
+
+            if (radix < 2 || radix > digits.Length)
+                throw new ArgumentException("The radix must be >= 2 and <= " + digits.Length);
+
+            if (number == 0)
+                return "0";
+
+            int index = bitsInLong - 1;
+            long currentNumber = Math.Abs(number);
+            char[] charArray = new char[bitsInLong];
+
+            while (currentNumber != 0)
+            {
+                int remainder = (int)(currentNumber % radix);
+                charArray[index--] = digits[remainder];
+                currentNumber = currentNumber / radix;
+            }
+
+            string result = new String(charArray, index + 1, bitsInLong - index - 1);
+            if (number < 0)
+            {
+                result = "-" + result;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Converts the given number from the numeral system with the specified
+        /// radix (in the range [2, 80]) to decimal numeral system.
+        /// </summary>
+        /// <param name="number">The arbitrary numeral system number to convert.</param>
+        /// <param name="radix">The radix of the numeral system the given number
+        /// is in (in the range [2, 80]).</param>
+        /// <returns></returns>
+        public static long ArbitraryToDecimalSystem(string number, int radix)
+        {
+            const string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/-*()[]<>?!^{}&$=";
+
+            if (radix < 2 || radix > digits.Length)
+                throw new ArgumentException("The radix must be >= 2 and <= " + digits.Length);
+
+            if (String.IsNullOrEmpty(number))
+                return 0;
+
+            // Make sure the arbitrary numeral system number is in upper case
+            //number = number.ToUpperInvariant();
+
+            long result = 0;
+            long multiplier = 1;
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                char c = number[i];
+                if (i == 0 && c == '-')
+                {
+                    // This is the negative sign symbol
+                    result = -result;
+                    break;
+                }
+
+                int digit = digits.IndexOf(c);
+                if (digit == -1)
+                    throw new ArgumentException(
+                        @"Invalid character in the arbitrary numeral system number",
+                        "number");
+
+                result += digit * multiplier;
+                multiplier *= radix;
+            }
+
+            return result;
+        }
+    }
+}
