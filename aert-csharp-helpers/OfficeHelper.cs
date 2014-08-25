18b7f0636479f8811d15660af11a429ec9a4e355
aert <dev.aert@gmail.com>
2014-08-25 21:37:24 +0200

    Added OfficeHelper, RobocopyWrapper.

---
+++ aert-csharp-helpers/OfficeHelper.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using aert_csharp_extensions;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Interop.Word;
+using ApplicationClass = Microsoft.Office.Interop.Word.ApplicationClass;
+using System.Diagnostics;
+
+namespace aert_csharp_helpers
+{
+    public class OfficeHelper
+    {
+        #region IsOffice
+
+        public static bool IsOffice(string fileIn)
+        {
+            return IsWord(fileIn) || IsPowerpoint(fileIn) || IsExcel(fileIn);
+        }
+
+        private static bool IsWord(string fileIn)
+        {
+            string ext = (Path.GetExtension(fileIn) ?? "").ToUpper();
+            return ext.Equals(".DOC") || ext.Equals(".DOCX");
+        }
+
+        private static bool IsPowerpoint(string fileIn)
+        {
+            string ext = (Path.GetExtension(fileIn) ?? "").ToUpper();
+            return ext.Equals(".PPT") || ext.Equals(".PPTX");
+        }
+
+        private static bool IsExcel(string fileIn)
+        {
+            string ext = (Path.GetExtension(fileIn) ?? "").ToUpper();
+            return ext.Equals(".XLS") || ext.Equals(".XLSX");
+        }
+
+        #endregion
+
+        public static bool ConvertToPdf(string fileIn, string fileOut)
+        {
+            if (IsWord(fileIn))
+                return ConvertWordToPdf(fileIn, fileOut);
+            if (IsPowerpoint(fileIn))
+                return ConvertPowerpointToPdf(fileIn, fileOut);
+            if (IsExcel(fileIn))
+                return ConvertExcelToPdf(fileIn, fileOut);
+            return false;
+        }
+
+        private static bool ConvertExcelToPdf(string fileIn, string fileOut)
+        {
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+            excelApp.Visible = false;
+            excelApp.DisplayAlerts = false;
+            Workbook workbook = null;
+            Workbooks workbooks = null;
+            try
+            {
+                workbooks = excelApp.Workbooks;
+                workbook = workbooks.Open(fileIn);
+
+                foreach (Worksheet ws in workbook.Worksheets.OfType<Worksheet>())
+                {
+                    ws.PageSetup.Orientation = XlPageOrientation.xlLandscape;
+                    ws.PageSetup.Zoom = false;
+                    ws.PageSetup.FitToPagesWide = 1;
+                    ws.PageSetup.FitToPagesTall = false;
+                }
+
+                workbook.ExportAsFixedFormat(XlFixedFormatType.xlTypePDF,
+                    fileOut,
+                    XlFixedFormatQuality.xlQualityStandard,
+                    true,
+                    true,
+                    Type.Missing,
+                    Type.Missing,
+                    false,
+                    Type.Missing);
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                if (workbook != null)
+                {
+                    workbook.Close(XlSaveAction.xlDoNotSaveChanges);
+                    while (Marshal.FinalReleaseComObject(workbook) != 0)
+                    {
+                    }
+                    workbook = null;
+                }
+                if (workbooks != null)
+                {
+                    workbooks.Close();
+                    while (Marshal.FinalReleaseComObject(workbooks) != 0)
+                    {
+                    }
+                    workbooks = null;
+                }
+                if (excelApp != null)
+                {
+                    excelApp.Quit();
+                    excelApp.Application.Quit();
+                    while (Marshal.FinalReleaseComObject(excelApp) != 0)
+                    {
+                    }
+                    excelApp = null;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool ConvertWordToPdf(string fileIn, string fileOut)
+        {
+            ApplicationClass wordApplication = new ApplicationClass();
+
+            Document wordDocument = null;
+
+            object paramSourceDocPath = fileIn;
+            object paramMissing = Type.Missing;
+
+            string paramExportFilePath = fileOut;
+
+            const WdExportFormat paramExportFormat = WdExportFormat.wdExportFormatPDF;
+            const bool paramOpenAfterExport = false;
+            const WdExportOptimizeFor paramExportOptimizeFor = WdExportOptimizeFor.wdExportOptimizeForPrint;
+            const WdExportRange paramExportRange = WdExportRange.wdExportAllDocument;
+            const int paramStartPage = 0;
+            const int paramEndPage = 0;
+            const WdExportItem paramExportItem = WdExportItem.wdExportDocumentContent;
+            const bool paramIncludeDocProps = true;
+            const bool paramKeepIrm = true;
+
+            const WdExportCreateBookmarks paramCreateBookmarks = WdExportCreateBookmarks.wdExportCreateWordBookmarks;
+
+            const bool paramDocStructureTags = true;
+            const bool paramBitmapMissingFonts = true;
+            // ReSharper disable once InconsistentNaming
+            const bool paramUseISO19005_1 = false;
+
+            try
+            {
+                // Open the source document.
+                wordDocument = wordApplication.Documents.Open(
+                    ref paramSourceDocPath, ref paramMissing, ref paramMissing,
+                    ref paramMissing, ref paramMissing, ref paramMissing,
+                    ref paramMissing, ref paramMissing, ref paramMissing,
+                    ref paramMissing, ref paramMissing, ref paramMissing,
+                    ref paramMissing, ref paramMissing, ref paramMissing,
+                    ref paramMissing);
+                //string tempFile = Path.Combine(Path.GetDirectoryName(fileOut), Path.GetFileName(fileIn));
+                //wordDocument.SaveAs(tempFile);
+                //wordDocument.Close();
+
+                // Export it in the specified format.
+                if (wordDocument != null)
+                    wordDocument.ExportAsFixedFormat(paramExportFilePath,
+                        paramExportFormat, paramOpenAfterExport,
+                        paramExportOptimizeFor, paramExportRange, paramStartPage,
+                        paramEndPage, paramExportItem, paramIncludeDocProps,
+                        paramKeepIrm, paramCreateBookmarks, paramDocStructureTags,
+                        paramBitmapMissingFonts, paramUseISO19005_1,
+                        ref paramMissing);
+            }
+            catch (Exception ex)
+            {
+                // Respond to the error
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                // Close and release the Document object.
+                if (wordDocument != null)
+                {
+                    wordDocument.Close(WdSaveOptions.wdDoNotSaveChanges, ref paramMissing,
+                        ref paramMissing);
+                    wordDocument = null;
+                }
+
+                // Quit Word and release the ApplicationClass object.
+                if (wordApplication != null)
+                {
+                    wordApplication.Quit(ref paramMissing, ref paramMissing,
+                        ref paramMissing);
+                    wordApplication = null;
+                }
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+
+            return true;
+        }
+
+        private static bool ConvertPowerpointToPdf(string fileIn, string fileOut)
+        {
+            Microsoft.Office.Interop.PowerPoint.Application ppApp =
+                new Microsoft.Office.Interop.PowerPoint.Application();
+            Microsoft.Office.Interop.PowerPoint.Presentation presentation = ppApp.Presentations.Open(fileIn,
+                Microsoft.Office.Core.MsoTriState.msoTrue, Microsoft.Office.Core.MsoTriState.msoFalse,
+                Microsoft.Office.Core.MsoTriState.msoFalse);
+
+            presentation.ExportAsFixedFormat(fileOut,
+                PpFixedFormatType.ppFixedFormatTypePDF,
+                PpFixedFormatIntent.ppFixedFormatIntentPrint,
+                Microsoft.Office.Core.MsoTriState.msoFalse,
+                PpPrintHandoutOrder.ppPrintHandoutHorizontalFirst,
+                PpPrintOutputType.ppPrintOutputSlides,
+                Microsoft.Office.Core.MsoTriState.msoFalse,
+                null,
+                PpPrintRangeType.ppPrintAll,
+                "",
+                false,
+                false,
+                false,
+                true,
+                false, // PDF-A
+                System.Reflection.Missing.Value);
+
+            presentation.Close();
+            presentation = null;
+            ppApp = null;
+            GC.Collect();
+            return true;
+        }
+    }
+}
---
+++ aert-csharp-helpers/RobocopyWrapper.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using aert_csharp_extensions;
+
+namespace aert_csharp_helpers
+{
+    /// <summary>
+    /// Classe utilitaire gérant les appels vers le programme Robocopy.
+    /// </summary>
+    public class RobocopyWrapper
+    {
+        public bool IsKilled { get; private set; }
+        private const string CMD = "robocopy.exe";
+        private const string CMD_OPTIONS = "/E /R:2 /W:15 /NS /NC /NJH /MIR";
+
+        public StringBuilder StandardOutputBuilder { get; private set; }
+        public StringBuilder StandardErrorBuilder { get; private set; }
+        public Process CopyProcess { get; private set; }
+
+        public object LockObject = new object();
+
+        #region Evénement à gérer
+
+        public event EventHandler OnOutputDataReceived;
+        public event EventHandler OnErrorDataReceived;
+
+        #endregion
+
+        public RobocopyWrapper()
+        {
+            StandardOutputBuilder = new StringBuilder();
+            StandardErrorBuilder = new StringBuilder();
+        }
+
+        #region Méthodes publiques
+
+        /// <summary>
+        /// Démarre une copie.
+        /// </summary>
+        public bool Copy(string src, string dest)
+        {
+            src = "\"{0}\"".HelpFormat(src);
+            dest = "\"{0}\"".HelpFormat(dest);
+
+            string args = "{0} {1} {2}".HelpFormat(src, dest, CMD_OPTIONS);
+
+            return StartProcess(args);
+        }
+
+        #endregion
+
+        public void Kill()
+        {
+            if (CopyProcess != null)
+                CopyProcess.Kill();
+            IsKilled = true;
+        }
+
+        #region Méthodes privées
+
+        private bool StartProcess(string args)
+        {
+
+            ProcessStartInfo ps = new ProcessStartInfo
+            {
+                FileName = CMD,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                Arguments = args,
+                StandardOutputEncoding = Encoding.GetEncoding(437),
+                StandardErrorEncoding = Encoding.GetEncoding(437),
+            };
+
+            try
+            {
+                using (Process p = Process.Start(ps))
+                {
+                    CopyProcess = p;
+
+                    p.OutputDataReceived += process_OutputDataReceived;
+                    p.ErrorDataReceived += process_ErrorDataReceived;
+
+                    p.BeginOutputReadLine();
+                    p.BeginErrorReadLine();
+                    p.WaitForExit();
+                    CopyProcess = null;
+                    return !IsKilled && (p.ExitCode >= 0 || p.ExitCode <= 2);
+                }
+            }
+            catch (Exception ex)
+            {
+                lock (LockObject)
+                {
+                    StandardErrorBuilder.Append(ex.Message);
+                }
+
+                RoboDataReceivedEventArgs eventArgs = new RoboDataReceivedEventArgs(ex.Message);
+                OnErrorDataReceived(this, eventArgs);
+                return false;
+            }
+        }
+
+        void process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            RoboDataReceivedEventArgs eventArgs = new RoboDataReceivedEventArgs(e.Data ?? string.Empty);
+
+            lock (LockObject)
+            {
+                if (IntProgress(eventArgs.Data) == null)
+                    StandardErrorBuilder.AppendLine(e.Data);
+            }
+
+            if (OnErrorDataReceived != null)
+                OnErrorDataReceived(sender, eventArgs);
+        }
+
+        void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            RoboDataReceivedEventArgs eventArgs = new RoboDataReceivedEventArgs(e.Data ?? string.Empty);
+
+            lock (LockObject)
+            {
+                if (IntProgress(eventArgs.Data) == null)
+                    StandardOutputBuilder.AppendLine(eventArgs.Data);
+            }
+
+            if (OnOutputDataReceived != null)
+                OnOutputDataReceived(sender, eventArgs);
+        }
+
+        #endregion
+
+        public float? IntProgress(string roboOutput)
+        {
+            roboOutput = roboOutput.Trim();
+            if (!roboOutput.EndsWith("%")) return null;
+
+            roboOutput = roboOutput.Replace("%", "").Replace(".", ",");
+            float rv;
+            if (float.TryParse(roboOutput, out rv))
+                return rv;
+            return null;
+        }
+    }
+
+    internal class RoboDataReceivedEventArgs : EventArgs
+    {
+
+        public string Data { get; set; }
+
+
+        public RoboDataReceivedEventArgs(string data)
+        {
+            Data = data;
+        }
+
+        public RoboDataReceivedEventArgs(DataReceivedEventArgs source)
+        {
+            Data = source.Data;
+        }
+    }
+}
