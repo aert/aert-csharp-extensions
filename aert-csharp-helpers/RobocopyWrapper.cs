2ad7d49de38f9addfe9a02cea9efb090a7a86232
aert <dev.aert@gmail.com>
2014-08-25 22:03:11 +0200

    Refactored usings.

--- aert-csharp-helpers/RobocopyWrapper.cs
+++ aert-csharp-helpers/RobocopyWrapper.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using aert_csharp_extensions;
 
 namespace aert_csharp_helpers
